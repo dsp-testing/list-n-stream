01248416514119d961672af915d08ded0acee196
Kory Nunn <knunn187@gmail.com>
2014-03-18 08:25:28 -0400

    Test changes

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ var pathHelpers = require('path');
 var beeline = require('beeline');
 var packageJson = require('./package');
 var program = require('commander');
+var querystring = require('querystring');
 
 
 program
@@ -30,18 +31,36 @@ function serveDir(request, response, path){
         response.end(
             '<a href="../">../</a><br>' +
             files.map(function(fileName){
-            return '<a href="' + fileName + '">' + fileName + '</a>';
+            return '<a href="' + fileName + '/?view">' + fileName + '</a>';
         }).join('<br>'));
     });
 }
 
+var mimeTypes = {
+    'avi': 'video/x-msvideo',
+    'mpg': 'video/mpeg',
+    'mpeg': 'video/mpeg',
+    'mp4': 'video/mpeg'
+}
+
 var router = beeline.route({
     '/`path...`': function(request, response, details){
-        var path = __dirname + '/' + details.path;
+        var path = process.cwd() + '/' + details.path;
         fs.stat(path, function(error, info){
+            if(error){
+                serverError(response, error);
+                return;
+            }
+            
             if(info.isDirectory()){
                 serveDir(request, response, path);
             }else{
+                var urlBits = request.url.split('?');
+                if('view' in querystring.parse(urlBits[1])){
+                    response.end('<video src="' + urlBits[0] + '" autoplay></video>')
+                    return;
+                }
+                response.writeHead(200, {"Content-Type": mimeTypes[path.split('.').pop()] || 'text/plain'});
                 fs.createReadStream(path)
                 .pipe(response);
             }
